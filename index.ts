77910b6ade441ab891690fcce36a9d629eb72a20
Jan Sommer <jan.sommer@hsrw.org>
2018-07-05 08:42:20 +0200

    Fix intendation

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 // all interfaces and helpers
-export {CognigyError} from "./src/interfaces/cognigyError";
-export {Options} from "./src/interfaces/options";
-export {Output} from "./src/interfaces/output";
+export { CognigyError } from "./src/interfaces/cognigyError";
+export { Options } from "./src/interfaces/options";
+export { IOutput } from "./src/interfaces/output";
 
 // the actual client
-export {CognigyClient} from "./src/client";
+export { CognigyClient } from "./src/client";
