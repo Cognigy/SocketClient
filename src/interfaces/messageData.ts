35c0691d950b80a0df8bda5521be2833c4f7ee08
Fabio Colajanni <f.colajanni@cognigy.com>
2024-02-14 13:57:15 +0100

    include plugin on message data

--- src/interfaces/messageData.ts
+++ src/interfaces/messageData.ts
@@ -22,7 +22,7 @@ export interface IMessage extends IWebchatClientMessage{
 
 export interface IMessageData {
 	_cognigy?: ICognigyData;
-	_plugin?: IPluginDatepicker | IPluginXApp | IPluginXAppSubmit;
+	_plugin?: IPluginDatepicker | IPluginXApp | IPluginXAppSubmit | IPluginAdaptiveCards;
 	attachments?: IUploadFileAttachmentData[];
 }
 
